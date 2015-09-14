73a47a7a65c9c1a4e04d7951d2ff549d24932de0
George Chang <georgec1104@gmail.com>
2015-09-14 14:49:22 +0800

    added relation between post and user. added strong parameter of User_id on Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, :category_ids => [])
+    params.require(:post).permit(:title, :content, :user_id, :category_ids => [])
   end
 
 private
