07b7b0f71cdd6439b708b0d4cc46cfdad2600205
George Chang <georgec1104@gmail.com>
2015-09-14 12:07:54 +0800

    adjusted sorting labels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,14 @@ module ApplicationHelper
   def sortable(sort_button, sort_button_name = nil)
     sort_button_name ||= sort_button.titleize
 
-    if sort_button.titleize == sort_button_name
+    if sort_button == sort_column
       css_class = "current #{sort_direction}"
     else
       css_class = nil
     end
 
-    # css_class = (sort_button == sort_button_name) ? "current #{sort_direction}" : nil
-    if sort_button == params[:sort] && params[:direction]=="asc"
+    # css_class = (sort_button == sort_column) ? "current #{sort_direction}" : nil
+    if sort_button == sort_column && sort_direction =="asc"
       direction = "desc"
     else
       direction = "asc"
